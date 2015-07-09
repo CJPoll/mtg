7cca1e781468db99b5a4d765eeffaeceeef19a26
Cody Poll <CJPoll@gmail.com>
2015-07-09 01:05:26 -0600

    Revert "Post to current channel"
    This reverts commit 8f47dc0a0cdd549ee28ea934c8001fa8018cb6cc.

--- app.rb
+++ app.rb
@@ -8,7 +8,6 @@ end
 
 post '/cards' do
 	card_name = params[:text]
-	channel = params[:channel]
 
 	base_uri = "https://magictgdeckpricer.firebaseio.com/allCards/"
 	firebase = Firebase::Client.new base_uri
@@ -16,7 +15,7 @@ post '/cards' do
 	response = firebase.get(card_path)
 	client = SlackNotify::Client.new(
 		webhook_url: "https://hooks.slack.com/services/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",
-		channel: channel
+		channel: '#testing-slashes'
 	)
 	client.notify("#{card_name}: #{response.body}")
 end
