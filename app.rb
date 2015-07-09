5a87cef2ed8ae550ca46c39f4cdd3fff3f09a056
Cody Poll <CJPoll@gmail.com>
2015-07-09 00:56:27 -0600

    Forgot to use response.body instead of just response

--- app.rb
+++ app.rb
@@ -12,7 +12,7 @@ post '/cards' do
 	base_uri = "https://magictgdeckpricer.firebaseio.com/allCards/"
 	firebase = Firebase::Client.new base_uri
 	card_path = "#{ card_name }.json"
-	response = JSON.parse(firebase.get(card_path))
+	response = JSON.parse(firebase.get(card_path).body)
 	client = SlackNotify::Client.new(
 		webhook_url: "https://hooks.slack.com/services/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",
 		channel: '#testing-slashes'
