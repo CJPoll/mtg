d8d9d66b1d85152d46554df40c3e0adba4bad589
Cody Poll <CJPoll@gmail.com>
2015-07-09 00:38:18 -0600

    Testing slack post

--- app.rb
+++ app.rb
@@ -12,11 +12,11 @@ post '/cards' do
 	base_uri = "https://magictgdeckpricer.firebaseio.com/allCards/"
 	firebase = Firebase::Client.new base_uri
 	card_path = "#{ card_name }.json"
-	response = firebase.get card_path
+	_response = firebase.get card_path
 	client = SlackNotify::Client.new(
 		webhook_url: "https://hooks.slack.com/services/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",
 		channel: '#testing-slashes'
 	)
-	client.notify(response.body)
+	client.test
 
 end
