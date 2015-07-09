6d0feaf180f2f08469ed1e869577e901022ee021
Cody Poll <CJPoll@gmail.com>
2015-07-09 01:10:56 -0600

    Use channel id instead of channel name

--- app.rb
+++ app.rb
@@ -8,7 +8,7 @@ end
 
 post '/cards' do
 	card_name = params[:text]
-	channel = params[:channel_name]
+	channel = params[:channel_id]
 
 	base_uri = "https://magictgdeckpricer.firebaseio.com/allCards/"
 	firebase = Firebase::Client.new base_uri
@@ -16,7 +16,7 @@ post '/cards' do
 	response = firebase.get(card_path)
 	client = SlackNotify::Client.new(
 		webhook_url: "https://hooks.slack.com/services/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",
-		channel: "##{channel}"
+		channel: "#{channel}"
 	)
 	client.notify("Channel: #{channel}\n#{card_name}: #{response.body}")
 end
