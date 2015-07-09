fbb74cf2a446f589dc7f15cfed248f8ba55d95e4
Cody Poll <CJPoll@gmail.com>
2015-07-09 00:32:09 -0600

    Responding with Firebase url

--- app.rb
+++ app.rb
@@ -11,11 +11,12 @@ post '/cards' do
 
 	base_uri = "https://magictgdeckpricer.firebaseio.com/allCards/"
 	firebase = Firebase::Client.new base_uri
-	response = firebase.get "#{ card_name }.json"
+	card_path = "#{ card_name }.json"
+	_response = firebase.get card_path
 	client = SlackNotify::Client.new(
 		webhook_url: "https://hooks.slack.com/services/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",
 		channel: '#testing-slashes'
 	)
-	client.notify(response.body)
+	client.notify("#{base_uri}#{card_path}")
 
 end
