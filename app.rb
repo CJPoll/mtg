3c0a0fb174532ea6b0f04812f7a1e3a862b9a6a2
Cody Poll <CJPoll@gmail.com>
2015-07-09 01:00:19 -0600

    Checking whether the call is even succeeding

--- app.rb
+++ app.rb
@@ -12,10 +12,10 @@ post '/cards' do
 	base_uri = "https://magictgdeckpricer.firebaseio.com/allCards/"
 	firebase = Firebase::Client.new base_uri
 	card_path = "#{ card_name }.json"
-	response = firebase.get(card_path).body
+	response = firebase.get(card_path)
 	client = SlackNotify::Client.new(
 		webhook_url: "https://hooks.slack.com/services/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",
 		channel: '#testing-slashes'
 	)
-	client.notify("#{card_name}: #{response.to_json}")
+	client.notify("#{card_name} successful?: #{response.success?}")
 end
