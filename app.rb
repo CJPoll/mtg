e0ca6004512e556b0ee3b1623c5a49c6f3cfd0c0
Cody Poll <cpoll@instructure.com>
2015-07-12 19:08:53 -0600

    Testing the set conversion

--- app.rb
+++ app.rb
@@ -7,16 +7,41 @@ get '/' do
 end
 
 post '/cards' do
-	card_name = params[:text]
-	channel = params[:channel_name]
+	card_name = params[ :text ]
+	channel = params[ :channel_name ]
 
-	base_uri = "https://magictgdeckpricer.firebaseio.com/allCards/"
-	firebase = Firebase::Client.new base_uri
-	card_path = "#{ card_name }"
-	response = firebase.get(card_path)
+	card_data = get_card_data(card_name)
+	set_data = get_set_data( card_name )
+
+	set_uri = 'https://magictgdeckpricer.firebaseio.com/setInfoX/'
+	firebase = Firebase::Client.new set_uri
+	encodedSet = URI::encode(set)
+	setBaseResponse = firebase.get(encodedSet)
+	code = setBaseResponse.body['code']
+
+
 	client = SlackNotify::Client.new(
-		webhook_url: "https://hooks.slack.com/services/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",
-		channel: "##{channel}"
+		webhook_url: 'https://hooks.slack.com/services/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'
 	)
-	client.notify("Channel: #{channel}\n#{card_name}: #{response.body}")
+	client.notify( "Channel: #{channel}\nSet Data: #{ setData }" )
+end
+
+def get_card_data( card_name )
+	card_path = "allCards/#{ card_name }/"
+	firebase.get( card_path )
+end
+
+def get_set_data( card_name )
+	set_path = 'setInfoX/'
+	firebase.get( set_path, { 
+		'orderBy' => 'name',
+		'startAt' => card_name,
+		endAt => card_name,
+		'limitToFirst' => 1 
+	})
+end
+
+def firebase
+	base_uri = "https://magictgdeckpricer.firebaseio.com/"
+	Firebase::Client.new base_uri
 end
