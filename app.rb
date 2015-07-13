ade24385fd75becad4c5f2a6f035c5c8241ab432
Jakobhartman <Jakobhartman@hotmail.com>
2015-07-12 22:18:31 -0500

    image grab

--- app.rb
+++ app.rb
@@ -10,13 +10,20 @@ post '/cards' do
 	card_name = params[:text]
 	channel = params[:channel_name]
 
-	base_uri = "https://magictgdeckpricer.firebaseio.com/allCards/"
-	firebase = Firebase::Client.new base_uri
-	card_path = "#{ card_name }"
-	response = firebase.get(card_path)
+	base_uri = "https://magictgdeckpricer.firebaseio.com/MultiverseTable/#{ card_name }/ids"
+
+	encode = URI::encode(base_uri)
+	puts encode 
+	firebase = Firebase::Client.new encode
+	response = firebase.get("","")
+	randomSet = 1 + rand(response.body.keys.length)
+	mId = response.body[response.body.keys[randomSet]]
+	uri = "http://gatherer.wizards.com/Handlers/Image.ashx?multiverseid=#{mId}&type=card"
+
 	client = SlackNotify::Client.new(
 		webhook_url: "https://hooks.slack.com/services/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",
+
 		channel: "##{channel}"
 	)
-	client.notify("Channel: #{channel}\n#{card_name}: #{response.body}")
+	client.notify("Channel: #{channel}\n#{card_name}: #{uri}")
 end
